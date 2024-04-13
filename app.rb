94dca220ce7243d8c8ca3354ba75aba4fd8dcd18
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-13 16:31:53 +0000

    commit

--- app.rb
+++ app.rb
@@ -3,34 +3,13 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
-
-
 get("/") do
 
-  Howdy
+  @api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  @raw_data = HTTP.get(@api_url)
+  @raw_data_string = @raw_data.to_s
+  @parsed_data = JSON.parse(@raw_data_string)
 
- end
-
-get("/:from_currency") do
-  @original_currency = params.fetch("from_currency")
-
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
-  
-  # some more code to parse the URL and render a view template
-end
-
-get("/:from_currency/:to_currency") do
-  @original_currency = params.fetch("from_currency")
-  @destination_currency = params.fetch("to_currency")
-
-  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
-  
-  # some more code to parse the URL and render a view template
-end
-
-
-
-
-
+  erb(:homepage)
 
 end
