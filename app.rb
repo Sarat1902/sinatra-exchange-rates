46409802e46c3167cf22337efc17a63b85c93dff
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-13 16:19:49 +0000

    commit

--- app.rb
+++ app.rb
@@ -1,9 +1,36 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  Howdy
+
+ end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  
+  # some more code to parse the URL and render a view template
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  # some more code to parse the URL and render a view template
+end
+
+
+
+
+
+
 end
